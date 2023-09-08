42e61284a6b8e1c47e27ab66da6c55bfc23dc885
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-09-08 05:43:43 +0530

    Updated main.py and vercel.json

--- main.py
+++ main.py
@@ -63,3 +63,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    
+'''
+{
+    "builds": [{
+        "src": "llmModelAi/llmModelAi/wsgi.py",
+        "use": "@vercel/python",
+        "config": { "maxLambdaSize": "15mb", "runtime": "python3.9" }
+    }],
+    "routes": [
+        {
+            "src": "/(.*)",
+            "dest": "llmModelAi/llmModelAi/wsgi.py"
+        }
+    ]
+}
+'''

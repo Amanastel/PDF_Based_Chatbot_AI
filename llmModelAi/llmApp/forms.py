9be48777e07ff02291720207049cd5269b89bb05
Aman Kumar <87702404+Amanastel@users.noreply.github.com>
2023-09-16 11:26:57 +0530

    Update forms.py added comment

--- llmModelAi/llmApp/forms.py
+++ llmModelAi/llmApp/forms.py
@@ -6,17 +6,38 @@ from .models import CustomProfile
 
 
 class RegistrationForm(forms.ModelForm):
+    """
+    Form for user registration.
+
+    Allows users to input their phone number and address when registering.
+
+    :param forms.ModelForm: Model form for user registration.
+    """
     class Meta:
         model = CustomProfile
         fields = ('phone', 'address')
 
 class PDFDocumentForm(forms.ModelForm):
+    """
+    Form for PDF document information.
+
+    Allows users to input the title and embedding when adding a PDF document.
+
+    :param forms.ModelForm: Model form for PDF document information.
+    """
     class Meta:
         model = PDFDocument
         fields = ['title', 'embedding']
         
        
 class PDFDocumentForm2(forms.ModelForm):
+    """
+    Form for updating PDF document information.
+
+    Allows users to edit the title, document content, and embedding of a PDF document.
+
+    :param forms.ModelForm: Model form for updating PDF document information.
+    """
     class Meta:
         model = PDFDocument
         fields = ['title', 'documentContent', 'embedding']
@@ -32,15 +53,36 @@ class PDFDocumentForm2(forms.ModelForm):
         } 
 
 class PDFUpdateForm(forms.ModelForm):
+    """
+    Form for updating PDF document information.
+
+    Allows users to edit the title, document content, and embedding of a PDF document.
+
+    :param forms.ModelForm: Model form for updating PDF document information.
+    """
     class Meta:
         model = PDFDocument
         fields = ['title', 'documentContent', 'embedding']
         
 class PDFUploadForm(forms.Form):
+    """
+    Form for uploading a PDF document.
+
+    Allows users to upload a PDF document.
+
+    :param forms.Form: Form for uploading a PDF document.
+    """
     pdf_document = forms.FileField(label='Upload a PDF', required=True)
     
 
 class UserQuestionForm(forms.Form):
+    """
+    Form for user questions.
+
+    Allows users to input their questions.
+
+    :param forms.Form: Form for user questions.
+    """
     user_question = forms.CharField(max_length=255, label='Your Question')
 
 # class ChatMessageForm(forms.ModelForm):
